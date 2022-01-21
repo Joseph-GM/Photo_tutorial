4e34f122c365dce52f6943f40338d7695802a7dd
joseph-GM <jkim.english75@gmail.com>
2022-01-21 13:46:46 +0900

    first commit

--- Photo_tutorial/ViewController.swift
+++ Photo_tutorial/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var profileChangeBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.profileImage.layer.cornerRadius = profileImage.frame.height / 2
+        self.profileChangeBtn.layer.cornerRadius = 10
+        self.profileChangeBtn.addTarget(self, action: #selector(onProfileChangeBtnClicked), for: .touchUpInside)
     }
 
 
+    @objc fileprivate func onProfileChangeBtnClicked() {
+        print("ViewController - onProfileChangeBtnClicked() called")
+    }
 }
 
